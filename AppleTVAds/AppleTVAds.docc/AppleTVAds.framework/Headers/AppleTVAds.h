c9772740868fcda1b48f8150d3b975782998a45c
Alex Bedulin <alexander.bedulin@gmail.com>
2023-12-27 15:04:45 +0100

    first commit

---
+++ AppleTVAds/AppleTVAds.docc/AppleTVAds.framework/Headers/AppleTVAds.h
@@ -0,0 +1,18 @@
+//
+//  AppleTVAds.h
+//  AppleTVAds
+//
+//  Created by alex on 27.12.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleTVAds.
+FOUNDATION_EXPORT double AppleTVAdsVersionNumber;
+
+//! Project version string for AppleTVAds.
+FOUNDATION_EXPORT const unsigned char AppleTVAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleTVAds/PublicHeader.h>
+
+
