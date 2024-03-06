324b93b0fddc3433b520f186124f0cbc709fa251
Alex Bedulin <alexander.bedulin@gmail.com>
2024-03-06 13:53:34 +0100

    final version 0.0.4

---
+++ AppleTVAds/ApiInterface.swift
@@ -0,0 +1,8 @@
+//
+//  ApiInterface.swift
+//  AppleTVAds
+//
+//  Created by alex on 29.02.24.
+//
+
+import Foundation
---
+++ AppleTVAds/TVAdsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TVAdsViewController.swift
+//  AppleTVAds
+//
+//  Created by alex on 29.02.24.
+//
+
+import Foundation
